50a11feab3b0e314ad58791267b917fe0c47c4c6
Dmitry Petrov <old.fightmaster@gmail.com>
2012-03-03 15:38:45 +0400

    Refactoring of vendors

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -9,17 +9,14 @@ if (isset($argv[1])) {
 
 $vendorDir = __DIR__;
 $deps = array(
-    array('symfony', 'https://github.com/symfony/symfony.git', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
-    array('sldao', 'https://github.com/fightmaster/SLDAO.git', 'origin/master'),
-    array('doctrine-common', 'https://github.com/doctrine/common.git', 'origin/master'),
-    array('doctrine-dbal', 'https://github.com/doctrine/dbal.git', 'origin/master'),
-    array('doctrine', 'https://github.com/doctrine/doctrine2.git', 'origin/master'),
-    array('doctrine-mongodb-odm', 'https://github.com/doctrine/mongodb-odm.git', 'origin/master'),
-    array('doctrine-mongodb', 'https://github.com/doctrine/mongodb.git', 'origin/master'),
-    /*array('doctrine-couchdb', 'http://github.com/doctrine/couchdb-odm.git', 'origin/master'),
-    array('propel', 'http://github.com/propelorm/Propel.git', 'origin/master'),
-    array('propel-behavior', 'http://github.com/willdurand/TypehintableBehavior.git', 'origin/master'),
-    array('phing', 'http://github.com/Xosofox/phing.git', 'origin/master'),*/
+    array('symfony', 'git://github.com/symfony/symfony.git', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
+    array('dao', 'git://github.com/fightmaster/dao.git', 'origin/master'),
+    array('twig', 'git://github.com/fabpot/Twig.git', 'origin/master'),
+    array('doctrine-common', 'git://github.com/doctrine/common.git', 'origin/master'),
+    array('doctrine-dbal', 'git://github.com/doctrine/dbal.git', 'origin/master'),
+    array('doctrine', 'git://github.com/doctrine/doctrine2.git', 'origin/master'),
+    array('doctrine-mongodb-odm', 'git://github.com/doctrine/mongodb-odm.git', 'origin/master'),
+    array('doctrine-mongodb', 'git://github.com/doctrine/mongodb.git', 'origin/master'),
 );
 
 foreach ($deps as $dep) {
